966aeb3b9d89316ab87a14c8c0f1a0494e914b3a
Tfg Developer <tfgebookdeveloper@gmail.com>
2026-05-20 21:39:39 +0500

    complete website final final

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -74,7 +74,7 @@ export default function Header() {
       >
         <div className="max-w-[1600px] mx-auto flex items-center justify-between h-16 px-4 md:px-8 py-2 mt-3">
 
-          {/* Logo */}
+          
           <Link
             to="/"
             className="flex items-center"
